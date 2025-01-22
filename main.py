9faf96fef3f34fc70a77a8196e79ad62c3e44b0b
n-reeves <nickwreeves@gmail.com>
2025-01-21 16:24:29 -0800

    Intiial Testing of inference pipeline and local api endpoint

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from typing import List
 from fastapi import FastAPI, status
 from pydantic import BaseModel
 import torch
+import numpy as np
 import os
 from utils import batch_stft, batch_istft, file_to_batch, batch_to_file
 
 MODEL_PATH = "ml/model"
-MODEL_NAME = "model.pth"
+MODEL_NAME = "model.pt"
 
-def load_model():
+class Payload(BaseModel):
+    audio: List[List[float]]
+
+def load_model() -> torch.jit._script.RecursiveScriptModule:
     """Load the model from the model_dir.
     Args:
         model_dir (str): path to the directory containing model.pth
@@ -19,29 +23,32 @@ def load_model():
     model = torch.jit.load(os.path.join(MODEL_PATH, MODEL_NAME))
     return model
 
-def preprocess(input_audio: _type_):
+def load_payload():
+    pass
+
+def preprocess(input_audio: List[List[float]]) -> torch.Tensor:
     """Preprocess the input audio.
 
     Args:
         input_audio (_type_): Open
 
     Returns:
-        torch.tensor: audio tensor of shape (Batch, 1, frequency bins, time frames, 2)
+        torch.Tensor: audio tensor of shape (Batch, 1, frequency bins, time frames, 2)
     """
     audio_tensor = torch.tensor(input_audio) #convert input audio to audio tensor
     audio_batch = file_to_batch(audio_tensor)
     stft_batch = batch_stft(audio_batch)
     return stft_batch
 
-def predict(model, input: torch.tensor):
+def predict(model: torch.jit._script.RecursiveScriptModule, input: torch.Tensor) -> torch.Tensor:
     """Predict the output using the model and input.
 
     Args:
         model (torch.jit.ScriptModule): traced model
-        input (torch.tensor): audio tensor of shape (Batch(variable), 1, 257, 256, 2)
+        input (torch.Tensor): audio tensor of shape (Batch(variable), 1, 257, 256, 2)
 
     Returns:
-        torch.tensor: torch.tensor of shape (Batch, 1, frequency bins, time frames)
+        torch.Tensor: tensor of shape (Batch, 1, frequency bins, time frames)
     """
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
@@ -49,27 +56,27 @@ def predict(model, input: torch.tensor):
     output = model(input)
     return output
 
-def postprocess(output: torch.tensor):
+def postprocess(output: torch.Tensor, input_length: int) -> np.ndarray:
     """Postprocess the output audio.
 
     Args:
-        output (torch.tensor): audio tensor of shape (Batch, 1, frequency bins, time frames)
+        output (torch.Tensor): audio tensor of shape (Batch, 1, frequency bins, time frames)
 
     Returns:
-        _type_: Open
+        _type_: List[List[floats]]
     """
     batch_wav = batch_istft(output)
     wav = batch_to_file(batch_wav)
-    wav = wav.detach().cpu().numpy()
-    return wav
+    wav = wav.detach()
+    if wav.device != torch.device('cpu'):
+        wav = wav.cpu()
+    wav = wav[:, :input_length]
+    return wav.tolist()
 
-def load_payload():
-    pass
+### Decorators
 
+#Reference note: to run server with uvicorn, type in terminal: uvicorn main:app --reload
 app = FastAPI()
-
-class Payload(BaseModel):
-    input: List[float] #what is this?
 
 #required formatting to create endpoint healthcheck  
 @app.get('/ping')
@@ -80,3 +87,56 @@ def ping():
 @app.post('/invocations')
 def invoke():
     pass
+
+
+### Test endpoint routes
+
+##test load model function
+@app.get('/test/load_model')
+def test_load_model():
+    model = load_model()
+    return {"model": str(model)}
+
+
+##test recieve payload
+#from python
+@app.post('/test/recieve_payload')
+def test_recieve_payload(input_audio: Payload):
+    return {'input': input_audio.audio}
+
+#from javascript (load payload)
+#pending
+
+##preprocess debug
+@app.post('/test/preprocessdebug')
+def test_preprocess(input_audio: Payload):
+    out = input_audio.audio
+    return {"output": out, "output_tensor": torch.tensor(out).tolist()}
+
+
+##test preprocess function
+@app.post('/test/preprocess')
+def test_preprocess(input_audio: Payload):
+    stft = preprocess(input_audio.audio)
+    return {"stft": stft.tolist()}
+
+
+##test predict function
+@app.post('/test/predict')
+def test_predict(input_audio: Payload):
+    model = load_model()
+    model_in = preprocess(input_audio.audio)
+    output = predict(model, model_in) #complex numbers not json serializable
+    return {"output":list(output.shape)}
+
+
+##test postprocess function
+@app.post('/test/postprocess')
+def test_postprocess(input_audio: Payload):
+    model = load_model()
+    model_in = preprocess(input_audio.audio)
+    output = predict(model, model_in)
+    wav = postprocess(output, len(input_audio.audio[0]))
+    return {"wav": wav}
+    
+
--- utils.py
+++ utils.py
@@ -1,17 +1,37 @@
 import torch
+import torchaudio
 import librosa
 import matplotlib.pyplot as plt
 
 
-def file_to_batch(wav, clip_length=32640):
+def load_wav(filepath: str, sr: int = 8000) -> torch.Tensor:
+    """Load the audio file from the filepath
+
+    Args:
+        filepath (str): _description_
+        sr (int, optional): sample rate. Defaults to 8000.
+
+    Returns:
+        torch.Tensor: tensor representation of audio file
+    """
+    audio, nat_sr = torchaudio.load(filepath) #dim: ch x samples 
+    if sr is not None and sr != nat_sr :
+        audio = torchaudio.functional.resample(audio, nat_sr, sr)
+    return audio
+
+def write_wav(filepath: str, audio: torch.Tensor, sr: int = 8000) -> None:
+    torchaudio.save(uri=filepath, src=audio, sample_rate=sr)
+
+
+def file_to_batch(wav: torch.Tensor, clip_length: int=32640) -> torch.Tensor:
     """Convert the input audio file to a batch of audio tensors.
 
     Args:
-        wav (torch.tensor): audio tensor of shape (1, file length in samples)
+        wav (torch.Tensor): audio tensor of shape (1, file length in samples)
         clip_length (int, optional): Length of each batch item in samples. Defaults to 32640.
 
     Returns:
-        torch.tensor: tensor of shape (Batch, 1, clip_length)
+        torch.Tensor: tensor of shape (Batch, 1, clip_length)
     """
     n = wav.shape[1]
     
@@ -22,17 +42,20 @@ def file_to_batch(wav, clip_length=32640):
     wav_batch = wav.reshape(-1,1,clip_length)
     return wav_batch
 
-def batch_stft(data, sr=8000, hop_len_s=.016, win_s=.064):
+def batch_stft(data: torch.Tensor, 
+               sr: int=8000, 
+               hop_len_s: float=.016, 
+               win_s: float=.064) -> torch.Tensor:
     """Transforms batched audio to STFT coefficients
 
     Args:
-        data (torch.tensor): audio tensor of shape (Batch, 1, clip_length)
+        data (torch.Tensor): audio tensor of shape (Batch, 1, clip_length)
         sr (int, optional): sample rate. Defaults to 8000.
         hop_len_s (float, optional): hop length in seconds. Defaults to .016.
         win_s (float, optional): window size in seconds. Defaults to .064.
 
     Returns:
-        torch.tensor: audio tensor of shape (Batch, 1, frequency bins, time frames, 2)
+        torch.Tensor: audio tensor of shape (Batch, 1, frequency bins, time frames, 2)
     """
     hl_sam = int(round(hop_len_s*sr))
     wl_sam = int(round(win_s*sr))
@@ -49,17 +72,20 @@ def batch_stft(data, sr=8000, hop_len_s=.016, win_s=.064):
     return stft_coefs.unsqueeze(1)
 
 
-def batch_istft(data, sr=8000, hop_len_s=.016, win_s=.064):
+def batch_istft(data: torch.Tensor, 
+                sr: int=8000, 
+                hop_len_s: float=.016, 
+                win_s: float=.064) -> torch.Tensor:
     """Converts batched audio from STFT coefficients to audio
 
     Args:
-        data (torch.tensor): complex tensor of shape (Batch, 1, frequency bins, time frames)
+        data (torch.Tensor): complex tensor of shape (Batch, 1, frequency bins, time frames)
         sr (int, optional): sample rate. Defaults to 8000.
         hop_len_s (float, optional): hop length in seconds. Defaults to .016.
         win_s (float, optional): window seize in seconds. Defaults to .064.
 
     Returns:
-        torch.tensor: torch tensor with shape (Batch, 1, clip_length)
+        torch.Tensor: torch tensor with shape (Batch, 1, clip_length)
     """
     hl_sam = int(round(hop_len_s*sr))
     wl_sam = int(round(win_s*sr))
@@ -73,19 +99,20 @@ def batch_istft(data, sr=8000, hop_len_s=.016, win_s=.064):
     
     return wavs.unsqueeze(1)
 
-def batch_to_file(batch):
+def batch_to_file(batch: torch.Tensor) -> torch.Tensor:
     """Converts batched audio to a single audio file
 
     Args:
-        batch (torch.tensor): audio tensor of shape (Batch, 1, clip_length)
+        batch (torch.Tensor): audio tensor of shape (Batch, 1, clip_length)
 
     Returns:
-        torch.tensor: audio tensor of shape (1, Batch*clip_length)
+        torch.Tensor: audio tensor of shape (1, Batch*clip_length)
     """
     wav = batch.reshape(1,-1)
     return wav
 
-#code from labs
+
+# Pending
 def spec_plot(audio, sr=8000, n_fft=512, hop_length=128, save_png=False, png_name='test.png'):
     X = librosa.stft(audio, n_fft=n_fft, hop_length=hop_length)
     S = librosa.amplitude_to_db(abs(X)) 
@@ -98,4 +125,7 @@ def spec_plot(audio, sr=8000, n_fft=512, hop_length=128, save_png=False, png_nam
         plt.savefig(png_name)
         
     plt.show()
+
+
+
     
